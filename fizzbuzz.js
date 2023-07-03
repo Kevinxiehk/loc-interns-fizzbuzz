9b04aaedbcd5141d45c82c5c926b7daf7ef143d7
Kevin Xie <kevinxiehk@gmail.com>
2023-07-03 17:04:28 +0100

    Implemented up to get specifying rules via command line options

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,11 @@
 function fizzbuzz_n(n) {
     for(let i = 1; i <= n; i++) {
         let result = [];
-        if(i % 3 == 0) result.push("Fizz");
-        if(i % 5 == 0) result.push("Buzz");
-        if(i % 7 == 0) result.push("Bang");
-        if(i % 11 == 0) result = ["Bong"];
-        if(i % 13 == 0) {
+        if(process.argv.indexOf('-3') > -1 && i % 3 == 0) result.push("Fizz");
+        if(process.argv.indexOf('-5') > -1 && i % 5 == 0) result.push("Buzz");
+        if(process.argv.indexOf('-7') > -1 && i % 7 == 0) result.push("Bang");
+        if(process.argv.indexOf('-11') > -1 && i % 11 == 0) result = ["Bong"];
+        if(process.argv.indexOf('-13') > -1 && i % 13 == 0) {
             for (let j = 0; j <= result.length; j++) {
                 if (j == result.length || result[j][0] == 'B') {
                     result.splice(j, 0, "Fezz");
@@ -13,7 +13,7 @@ function fizzbuzz_n(n) {
                 }
             }
         }
-        if(i % 17 == 0) result.reverse();
+        if(process.argv.indexOf('-17') > -1 && i % 17 == 0) result.reverse();
         if(result.length == 0) result = [i];
         console.log(result.join(""));
     }
@@ -25,7 +25,6 @@ function fizzbuzz() {
         input: process.stdin,
         output: process.stdout,
     });
-    let max;
     readline.question('How many numbers do you want?\n', answer => {
         fizzbuzz_n(Number(answer));
         readline.close();
